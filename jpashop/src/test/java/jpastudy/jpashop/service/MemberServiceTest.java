6cd5d1c286dedd96c2df9b7d02f21c7423a2648d
cmsxi <cms0755@naver.com>
2024-02-06 19:19:18 +0900

    test: 멤버 테스트 코드 생성

---
+++ jpashop/src/test/java/jpastudy/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,34 @@
+package jpastudy.jpashop.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class) // 단위테스트를 만드는게 아니라 JPA가 실제로 DB까지 도는 걸 보이기 위해 메모리 모드로 엮으려고 사용.
+    // 완전히 Spring test 통합
+class MemberServiceTest {
+// 2분부터 다시
+
+    // 회원가입
+    @Test
+    public void 회원가입() throws Exception {
+        // given
+
+        // when
+
+        // then
+    }
+
+    // 중복 회원 예외
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        // given
+
+        // when
+
+        // then
+    }
+
+}
